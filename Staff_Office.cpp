9898fb44bbcb91465c8899dbd8a5e17b6a544197
NhatLam <luonghoangnhatlam2022@gmail.com>
2026-04-02 15:02:13 +0700

    add file cpp

--- Staff_Office.cpp
+++ Staff_Office.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std; 
 struct office {
-    int id;
-    string name;
-    string date ;
-    long double salary;
+    int id; 
+    string name; 
+    string date ; 
+    long double salary; 
 };
 void nhap_nhan_vien(office a){
     cout << "nhap ten nhan vien : ";
@@ -17,7 +17,7 @@ void nhap_nhan_vien(office a){
     cin >> a.salary;
 }
 void xuat_nhan_vien(office a){
-    cout << "ten nhan viên: " << a.name;
+    cout << "ten nhan viÃªn: " << a.name;
     cout << "id nhan vien  : " << a.id;
     cout << "ngay sinh nhan vien : " << a.date;
     cout << "tien luong cua nhan vien : " << a.salary;
@@ -25,18 +25,18 @@ void xuat_nhan_vien(office a){
 }
 int main (){
     office a[1000];
-    int n ;
-    cout << "moi nhap so nhan vien : ";
-    cin >> n ;
+    int n ; 
+    cout << "moi nhap so nhan vien : "; 
+    cin >> n ; 
     for (int i=0;i<n;i++){
         cout << "Moi Nhap Thong Tin Cua Nhan Vien THu ", i+1 ;
-        nhap_nhan_vien(a[i]);
+        nhap_nhan_vien(a[i]); 
     }
     for (int i=0;i<n;i++){
         cout << "staff information" << '\n';
-        cout << "nhan vien thu: ", i+1;
+        cout << "nhan vien thu: ", i+1; 
         xuat_nhan_vien(a[i]);
     }
 
-    return 0 ;
+    return 0 ; 
 }
--- Xu_Li_So_Lon.cpp
+++ Xu_Li_So_Lon.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+/*#include <bits/stdc++.h>
 using namespace std;
 void add(char a[],char b[]);
 void reverse(int a[],int n);
@@ -22,4 +22,75 @@ void add(char a[],char b[]){
 }
 void reverse(int a[],int n){
 
+}
+*/
+#include <bits/stdc++.h>
+using namespace std ;
+char doi_so_sang_ki_tu(int n){
+    char KiTu = n + '0';
+    return KiTu; 
+}
+int doi_ki_tu_sang_so(char n){
+    int So = n - '0'; 
+    return So; 
+}
+string cong_hai_so_lon(string a,string b){ // a > b 
+    for (int i=b.size();i < a.size();i++) b = "0" + b ; 
+
+    int nho=0;
+    int example; 
+    string result; 
+    for (int i=a.size()-1;i>=0;i--){
+        
+        example = doi_ki_tu_sang_so(a[i])+doi_ki_tu_sang_so(b[i]) + nho ; 
+        nho = example/10 ;
+        result = doi_so_sang_ki_tu(example % 10) + result ; 
+
+    }
+
+    if ( nho > 0 ) {
+        result = doi_so_sang_ki_tu(nho) + result ;
+    }
+    return result ; 
+
+}
+string tru_hai_so_lon(string a,string b){
+    for (int i=b.size();i < a.size();i++) b="0"+b ; 
+
+    int nho=0; 
+    int example ; 
+    string result;
+    for (int i = a.size()-1;i>=0;i--){
+        if(a[i]>=b[i]){
+            example = doi_ki_tu_sang_so(a[i]) - (doi_ki_tu_sang_so(b[i]) + nho);
+            nho=0; 
+        }
+        else
+        {
+            example = ( doi_ki_tu_sang_so(a[i]))+ 10  - doi_ki_tu_sang_so(b[i]) - nho ;
+            nho=1;
+        }
+
+        result = doi_so_sang_ki_tu( example ) + result ; 
+    }
+    
+    if ( nho > 0  ) {
+        result = doi_so_sang_ki_tu(nho) + result ;        
+    }
+    int i=0; 
+    while(result[i]=='0'){
+        result.erase(0,1);
+    }
+    return result ; 
+    
+}
+int main (){
+    string a; 
+    string b; 
+    cin >> a ; 
+    cin >> b ; 
+    cout << cong_hai_so_lon(a,b); 
+    cout << endl; 
+    cout<< tru_hai_so_lon(a,b);
+    return 0; 
 }
